f5147bb1d75dca4fd2b853053945e60802b9708f
Nadav Abrahami <nadav_ab@wix.com>
2022-01-03 00:37:06 +0200

    a

--- packages/mostly-equal/src/test/safe-print.unit.ts
+++ packages/mostly-equal/src/test/safe-print.unit.ts
@@ -28,7 +28,7 @@ describe('safe print', () => {
       obj.b.push(obj as never);
       const expected = {
         a: 'a string',
-        b: ['circular data removed, path: actual["b"][0]'],
+        b: ['circular data removed, path: "actual[\\"b\\"][0]"'],
       };
       const actual = safePrint(obj);
       expect(actual).to.equal(JSON.stringify(expected, null, 2));
