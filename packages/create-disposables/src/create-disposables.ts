952ce0446720428d916de6bfbc682a7305644b41
Barak Igal <baraki@wix.com>
2022-03-03 17:37:13 +0200

    refactor!: cleanup disposables

--- packages/create-disposables/src/create-disposables.ts
+++ packages/create-disposables/src/create-disposables.ts
@@ -3,19 +3,22 @@ export function createDisposables() {
 
   return {
     async dispose() {
-      const toDispose = new Set(Array.from(disposables).reverse());
+      const toDispose = Array.from(disposables).reverse();
       disposables.clear();
       for (const dispose of toDispose) {
         await dispose();
       }
     },
-    add<T extends { dispose(): unknown } | (() => unknown)>(disposable: T): T {
+    add(disposable: Disposable) {
       if (typeof disposable === 'function') {
         disposables.add(disposable);
       } else {
         disposables.add(() => disposable.dispose());
       }
-      return disposable;
     },
   };
 }
+
+export type DisposeFunction = () => unknown;
+export type Disposable = { dispose: DisposeFunction } | DisposeFunction;
+export type Disposables = ReturnType<typeof createDisposables>;
