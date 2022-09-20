00ed9996e7194791faf9fed586836ebf1750451f
Omry Nachman <omry@wix.com>
2022-09-20 14:05:30 +0200

    added optional blocking behaviour

--- packages/create-disposables/src/create-disposables.ts
+++ packages/create-disposables/src/create-disposables.ts
@@ -1,22 +1,43 @@
 export function createDisposables() {
-  const disposables = new Set<() => unknown>();
+  const disposables = new Set<{dispose:() => unknown, blocking:boolean}>();
 
   return {
     async dispose() {
       const toDispose = Array.from(disposables).reverse();
       disposables.clear();
-      await Promise.all(toDispose.map(d => d()))     
+      for (const d of toDispose) {
+        d.blocking ? await d.dispose() : d.dispose()        
+      }
     },
     add(disposable: Disposable) {
       if (typeof disposable === 'function') {
-        disposables.add(disposable);
+        disposables.add({
+          dispose: () => (disposable(), disposable.toString()),
+          blocking: false
+        });
       } else {
-        disposables.add(() => disposable.dispose());
+        disposables.add({
+          dispose: () => (disposable.dispose(), disposable.id),
+          blocking: false
+        });
+      }
+    },
+    addBlocking(disposable: Disposable) {
+      if (typeof disposable === 'function') {
+        disposables.add({
+          dispose: disposable,
+          blocking: true
+        });
+      } else {
+        disposables.add({
+          dispose: () => disposable.dispose(),
+          blocking: true
+        });
       }
     },
   };
 }
 
 export type DisposeFunction = () => unknown;
-export type Disposable = { dispose: DisposeFunction } | DisposeFunction;
+export type Disposable = { dispose: DisposeFunction, id: string } | DisposeFunction;
 export type Disposables = ReturnType<typeof createDisposables>;
