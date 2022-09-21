f1a8c442dbb2927200c6674393ccfaaa4973d7c9
Omry Nachman <omry@wix.com>
2022-09-21 10:41:40 +0200

    Added timeout

--- packages/create-disposables/src/create-disposables.ts
+++ packages/create-disposables/src/create-disposables.ts
@@ -1,43 +1,40 @@
 export function createDisposables() {
-  const disposables = new Set<{dispose:() => unknown, blocking:boolean}>();
+  const disposables = new Set<{
+    disposable: Disposable, timeout: number
+  }>();
 
   return {
     async dispose() {
       const toDispose = Array.from(disposables).reverse();
       disposables.clear();
-      for (const d of toDispose) {
-        d.blocking ? await d.dispose() : d.dispose()        
+      for (const { disposable, timeout } of toDispose) {
+        const start = performance.now()
+        await disposable.dispose()
+        const duration = performance.now() - start
+        if (duration > timeout) {
+          throw new Error(`Timeout disposing of ${disposable.id}`)
+        }
       }
     },
-    add(disposable: Disposable) {
+
+    add(disposable: Disposable | DisposeFunction, timeout = 5000) {
       if (typeof disposable === 'function') {
         disposables.add({
-          dispose: () => (disposable(), disposable.toString()),
-          blocking: false
+          disposable: {
+            dispose: () => disposable(),
+            id: disposable.toString()
+          },
+          timeout
         });
       } else {
         disposables.add({
-          dispose: () => disposable.dispose(),
-          blocking: false
+          disposable,
+          timeout
         });
       }
-    },
-    addBlocking(disposable: Disposable) {
-      if (typeof disposable === 'function') {
-        disposables.add({
-          dispose: disposable,
-          blocking: true
-        });
-      } else {
-        disposables.add({
-          dispose: () => disposable.dispose(),
-          blocking: true
-        });
-      }
-    },
+    }
   };
-}
+};
 
 export type DisposeFunction = () => unknown;
-export type Disposable = { dispose: DisposeFunction } | DisposeFunction;
-export type Disposables = ReturnType<typeof createDisposables>;
+export type Disposable = { dispose: DisposeFunction, id: string }
