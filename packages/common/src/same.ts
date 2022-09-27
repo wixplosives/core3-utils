d08cac435d12d844cb90a0d49ec18ab871102028
Omry Nachman <omry@wix.com>
2022-09-27 11:52:20 +0200

    improved same

--- packages/common/src/same.ts
+++ packages/common/src/same.ts
@@ -4,30 +4,43 @@ import { isMap, isSet } from "./types"
 
 /**
  * deep comparison of two items
+ * items are "the same" if:
+ * - a === b (for anything other than iterables & POJO)
+ * - a and b are POJO with same entries (order ignored, "same" used to compare values)
+ * - a and b are Maps with same entries (order ignored, "same" used to compare keys & values)
+ * - a and b are Sets with same values (order ignored, "same" used to compare values)
+ * - a and b are iterable (that are not Set or Map) with the same values, order checked if unordered=false (default)
  * @param a 
  * @param b 
- * @param unordered Note: relevant only in array like. objects, sets and maps are always not unordered 
+ * @param unordered [false] Note: relevant only in array like iterables. objects, sets and maps are *never checked for order* of entries
  * @returns 
  */
 export function same<T>(a: T, b: T, unordered = false): boolean {
+    if (a === b) return true
     if (!isIterable(a) || !isIterable(b)) {
         if (isPlainObject(a) && isPlainObject(b)) {
-            return same(Object.entries(a), Object.entries(b), false)
+            return sameIgnoreOrder(Object.entries(a), Object.entries(b), unordered)
         }
         return a === b
     }
     return unordered ||
         (isMap(a) && isMap(b)) ||
         (isSet(a) && isSet(b))
-        ? sameIgnoreOrder(a, b) : sameInOrder(a, b)
+        ? sameIgnoreOrder(a, b, unordered) : sameInOrder(a, b, unordered)
 }
 
-function sameInOrder<T>(a: Iterable<T>, b: Iterable<T>) {
+function sameInOrder<T>(a: T, b: T, unordered:boolean) {
+    if (!isIterable(a) || !isIterable(b)) {
+        if (isPlainObject(a) && isPlainObject(b)) {
+            return sameIgnoreOrder(Object.entries(a), Object.entries(b), unordered)
+        }
+        return a === b
+    }
     const itA = a[Symbol.iterator]()
     const itB = b[Symbol.iterator]()
 
     for (let [aa, bb] = [itA.next(), itB.next()]; ; [aa, bb] = [itA.next(), itB.next()]) {
-        if (!same(aa.value, bb.value)) {
+        if (!same(aa.value, bb.value, unordered)) {
             return false
         }
         if (aa.done !== bb.done) {
@@ -39,7 +52,7 @@ function sameInOrder<T>(a: Iterable<T>, b: Iterable<T>) {
     }
 }
 
-function sameIgnoreOrder<T>(a: Iterable<T>, b: Iterable<T>) {
+function sameIgnoreOrder<T>(a: Iterable<T>, b: Iterable<T>, unordered:boolean) {
     const [aa, bb] = [histogram(a), histogram(b)]
     if (size(aa) !== size(bb)) {
         return false
@@ -47,7 +60,7 @@ function sameIgnoreOrder<T>(a: Iterable<T>, b: Iterable<T>) {
     for (const [key, count] of aa) {
         let wasFound = false
         for (const [bkey, bcount] of bb) {
-            if (same(key,bkey)) {
+            if (sameInOrder(key,bkey, unordered)) {
                 wasFound = true
                 if (bcount !== count) {
                     return false
--- packages/common/src/test/same.unit.ts
+++ packages/common/src/test/same.unit.ts
@@ -6,7 +6,7 @@ import { same } from "../same"
 import { toMap } from "../types"
 
 describe('same', () => {
-    describe('ignoreOrder = true', () => {
+    describe('types unaffected by "unordered" param', ()=>{
         it('compares native values', () => {
             expect(same(1, 1)).to.eql(true)
             expect(same(1, 0)).to.eql(false)
@@ -21,7 +21,7 @@ describe('same', () => {
             expect(same({ a: 1 }, { a: 2 })).to.eql(false)
             expect(same({ a: undefined }, {})).to.eql(false)
             expect(same({ a: undefined }, { a: null })).to.eql(false)
-            expect(same({ a: 1, b: 2 }, { b: 2, a: 1 })).to.eql(false)
+            expect(same({ a: 1, b: 2 }, { b: 2, a: 1 })).to.eql(true)
             expect(same({ a: { b: 1 } }, { a: { b: 1 } })).to.eql(true)
             expect(same({ a: { b: 1 } }, { a: { b: 2 } })).to.eql(false)
         })
@@ -41,6 +41,39 @@ describe('same', () => {
             expect(same(new Set([1]), new Set([1]))).to.eql(true)
             expect(same(new Set([1, 2, 3]), new Set([1, 2, 3]))).to.eql(true)
             expect(same(new Set([1]), new Set([2]))).to.eql(false)
+        })
+    })
+    describe('ordered', () => {
+        it('compares flat arrays', () => {
+            expect(same([],[])).to.eql(true)
+            expect(same([1,2],[1,2])).to.eql(true)
+            expect(same([1,2],[2,1])).to.eql(false)
+        })
+        it('compares iterables', () => {
+            expect(same([1,2][Symbol.iterator](),[1,2][Symbol.iterator]())).to.eql(true)
+            expect(same([1,2][Symbol.iterator](),[2,1][Symbol.iterator]())).to.eql(false)
+        })
+        it('compares nested arrays', () => {
+            expect(same([[[]]],[[[]]])).to.eql(true)
+            expect(same([1,[2,3]],[1,[2,3]])).to.eql(true)
+            expect(same([1,[3,2]],[1,[3,2]])).to.eql(true)
+        })
+        it('ignores the order within objects, maps and sets', ()=>{
+            expect(same([{b:0, a:1}],[{a:1, b:0}])).to.eql(true)
+            expect(same([toMap({b:0, a:1})],[toMap({a:1, b:0})])).to.eql(true)
+            expect(same([new Set([1,2])],[new Set([2,1])])).to.eql(true)
+        })
+        it('checks the order of arrays within objects', ()=>{
+            expect(same([{a:[0,1]}],[{a:[1,0]}])).to.eql(false)
+        })
+    })
+    describe('unordered array', ()=>{
+        it('ignores order of flat arrays', ()=>{
+            expect(same([0,1,2],[2,1,0], true)).to.eql(true)
+            expect(same([0,1,2],[2,1,0], true)).to.eql(true)
+        })
+        it('ignores order of nested arrays', ()=>{
+            expect(same([0,[1,2]],[[2,1],0], true)).to.eql(true)
         })
     })
 })
