d54efb813cb193dec9e8c99195fde44977ab2c9c
Omry Nachman <omry@wix.com>
2022-10-13 14:43:52 +0200

    fixed typing issue

--- packages/common/src/maps.ts
+++ packages/common/src/maps.ts
@@ -1,11 +1,20 @@
 import type { Nullable } from "./types"
 
-export const getValue = <K,V,T extends Map<K,V>>(map:Nullable<T>, key:K, errorMessage?:string):V => {
+type MapValue<T> = T extends Map<infer _, infer V> ? V : never
+
+/**
+ * Returns a value by key, throws if the value is missing or the map null
+ * @param map 
+ * @param key 
+ * @param errorMessage 
+ * @returns 
+ */
+export const getValue = <T extends Map<K,V>,K,V>(map:Nullable<T>, key:K, errorMessage?:string):MapValue<T> => {
     if (!map) {
         throw new Error(errorMessage || `Missing map`)
     }
     if (map && map.has(key)) {
-        return map.get(key)!
+        return map.get(key) as MapValue<T>
     } else {
         // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
         throw new Error(errorMessage || `Missing key: ${key}`)
