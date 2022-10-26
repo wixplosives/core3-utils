354dd892ae58f40d7063681fa756648190b64881
Omry Nachman <omry@wix.com>
2022-10-26 21:01:25 +0300

    fix: major! added tests, found and fixed some bugs removed event emitter duplications

--- packages/patterns/src/debouncer.ts
+++ packages/patterns/src/debouncer.ts
@@ -1,52 +1,94 @@
+import { deferred } from 'promise-assist';
+/**
+ * Cancelable debouncing of calls to trigger
+
+ * @example <caption>waitTime</caption>
+ * ```ts
+ * const debounced = new Debouncer(a => console.log(a), 1, 100)
+ * debounce.trigger('first')
+ * debounce.trigger('second')
+ * // after 1ms
+ * // => 'second'
+ * ```
+ * @example <caption>maxWaitTime</caption>
+ * ```ts
+ * const debounced = new Debouncer(a => console.log(a), 3, 4)
+ * debounce.trigger('first')
+ * // after 1ms
+ * debounce.trigger('second')
+ * // after 1ms
+ * // => 'second'
+ * debounce.trigger('third')
+ * // after 2ms, 4ms elapsed since the first trigger
+ * // => 'third'
+ * ```
+ */
 export class Debouncer<T extends (...args: unknown[]) => unknown> {
     private timeout: number | undefined;
     private maxTimeout: number | undefined;
-    constructor(
-        private cb: T,
-        private waitTime: number,
-        private maxWaitTime: number,
-        private _setTimeout: (cb: () => void, wait: number) => number = (...args) =>
-            // eslint-disable-next-line
-            setTimeout(...args),
-        private _clearTimeout: (id: number) => void = (id) =>
-            // eslint-disable-next-line
-            clearTimeout(id)
-    ) {}
-    trigger(...args: Parameters<T>) {
-        return new Promise<ReturnType<T>>((res, rej) => {
-            if (this.timeout) {
-                this._clearTimeout(this.timeout);
-            }
-            this.timeout = this._setTimeout(() => {
-                try {
-                    res(this.cb(...args) as ReturnType<T>);
-                } catch (ex) {
-                    rej(ex);
-                }
-                if (this.maxTimeout) {
-                    this._clearTimeout(this.maxTimeout);
-                }
-            }, this.waitTime);
-            if (!this.maxTimeout) {
-                this.maxTimeout = this._setTimeout(() => {
-                    try {
-                        res(this.cb(...args) as ReturnType<T>);
-                    } catch (ex) {
-                        rej(ex);
-                    }
-                    if (this.timeout) {
-                        this._clearTimeout(this.timeout);
-                    }
-                }, this.maxWaitTime);
-            }
-        });
+    private args = [] as any as Parameters<T>;
+    private defer = deferred<ReturnType<T>>();
+    /**
+     *
+     * @param cb - trigger callback
+     * @param waitTime - time to wait before invoking cb
+     * @param maxWaitTime - maximum wait time from first triggering
+     */
+    constructor(private cb: T, private waitTime: number, private maxWaitTime: number) {}
+
+    private execute() {
+        try {
+            const result = this.cb(...this.args) as ReturnType<T>;
+            this.defer.resolve(result);
+        } catch (ex) {
+            this.defer.reject(ex);
+        }
+        this.defer = deferred();
     }
+
+    /**
+     *
+     * @param args - arguments to pass to cb
+     * @returns Promised resolved with cb return value
+     */
+    trigger(...args: Parameters<T>) {
+        this.args = args;
+
+        if (this.timeout) {
+            clearTimeout(this.timeout);
+        }
+        this.timeout = setTimeout(() => {
+            this.execute();
+            if (this.maxTimeout) {
+                clearTimeout(this.maxTimeout);
+            }
+        }, this.waitTime);
+        if (!this.maxTimeout) {
+            this.maxTimeout = setTimeout(() => {
+                this.execute();
+                if (this.timeout) {
+                    clearTimeout(this.timeout);
+                }
+            }, this.maxWaitTime);
+        }
+        return this.defer.promise;
+    }
+    /**
+     * Cancels pending invocation of cb
+     * @example
+     * ```ts
+     * const debounced = new Debouncer(a => console.log(a), 3, 4)
+     * debounce.trigger('first')
+     * debounce.cancel()
+     * // console.log will not be invoked
+     * ```
+     */
     cancel() {
         if (this.timeout) {
-            this._clearTimeout(this.timeout);
+            clearTimeout(this.timeout);
         }
         if (this.maxTimeout) {
-            this._clearTimeout(this.maxTimeout);
+            clearTimeout(this.maxTimeout);
         }
     }
 }
--- packages/patterns/src/test/debounce.unit.ts
+++ packages/patterns/src/test/debounce.unit.ts
@@ -1,76 +1,73 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
+import { spy, SinonFakeTimers, useFakeTimers, SinonSpy } from 'sinon';
 import { Debouncer } from '../debouncer';
-import { reportError } from '@wixc3/common';
 
 describe('Debounce', () => {
-    class MockTimeout {
-        private counter = 0;
-        private cbCounter = 0;
-        private cbMap: Map<number, { cb: () => void; time: number }> = new Map();
-        setTimeout = (cb: () => void, wait: number) => {
-            this.cbCounter++;
-            this.cbMap.set(this.cbCounter, {
-                cb,
-                time: this.counter + wait,
-            });
-            return this.cbCounter;
-        };
-        clearTimeOut = (id: number) => {
-            this.cbMap.delete(id);
-        };
-        tick(num = 1) {
-            this.counter += num;
-            for (const [id, { cb, time }] of this.cbMap.entries()) {
-                if (this.counter === time) {
-                    cb();
-                    this.cbMap.delete(id);
-                }
-            }
-        }
-    }
+    let promises: Promise<any>[];
+    let clock: SinonFakeTimers;
+    let callback: SinonSpy;
+
+    beforeEach(() => {
+        promises = [];
+        callback = spy();
+        clock = useFakeTimers();
+    });
+    afterEach(async function () {
+        this.timeout(100);
+        clock.tick(100_000);
+        clock.restore();
+        await Promise.all(promises);
+    });
 
     it('should delay call by requested timeout', () => {
-        const spy = sinon.spy();
-        const timeoutMock = new MockTimeout();
-        const throttle = new Debouncer(spy, 1, 100, timeoutMock.setTimeout, timeoutMock.clearTimeOut);
-        throttle.trigger().catch(reportError);
-        expect(spy).to.have.not.been.called;
-        timeoutMock.tick();
-        expect(spy.callCount).to.equal(1);
+        const throttle = new Debouncer(callback, 1, 100);
+        promises.push(throttle.trigger());
+        expect(callback).to.have.callCount(0);
+        clock.tick(1);
+        expect(callback).to.have.callCount(1);
     });
     it('should delay calls while there are calls coming, call cb only once', () => {
-        const spy = sinon.spy();
-        const timeoutMock = new MockTimeout();
-        const throttle = new Debouncer(spy, 2, 100, timeoutMock.setTimeout, timeoutMock.clearTimeOut);
-        throttle.trigger().catch(reportError);
-        timeoutMock.tick();
-        throttle.trigger().catch(reportError);
-        timeoutMock.tick();
-        throttle.trigger().catch(reportError);
-        timeoutMock.tick();
-        throttle.trigger().catch(reportError);
-        expect(spy).to.have.not.been.called;
-        timeoutMock.tick(2);
-        expect(spy.callCount).to.equal(1);
+        const throttle = new Debouncer(callback, 2, 100);
+        promises.push(throttle.trigger());
+        clock.tick(1);
+        promises.push(throttle.trigger());
+        clock.tick(1);
+        promises.push(throttle.trigger());
+        clock.tick(1);
+        promises.push(throttle.trigger());
+        expect(callback).to.have.callCount(0);
+        clock.tick(2);
+        expect(callback).to.have.callCount(1);
     });
     it('if delaying call for more than max time, should trigger cb, go back to delaying', () => {
-        const spy = sinon.spy();
-        const timeoutMock = new MockTimeout();
-        const throttle = new Debouncer(spy, 2, 6, timeoutMock.setTimeout, timeoutMock.clearTimeOut);
+        const maxTimeout = 5;
+        const throttle = new Debouncer(callback, 3, maxTimeout);
+        for (let i = 0; i < maxTimeout + 1; i++) {
+            promises.push(throttle.trigger());
+            clock.tick(1);
+        }
+        expect(callback.callCount).to.equal(1);
+        clock.tick(2);
+        expect(callback.callCount).to.equal(2);
+    });
+    it(`passes the last trigger's arguments at waitTime`, () => {
+        const throttle = new Debouncer(callback, 2, 6);
+        promises.push(throttle.trigger(0));
+        clock.tick(1);
+        promises.push(throttle.trigger(1));
+        expect(callback).to.have.callCount(0);
+        clock.tick(1);
+        clock.tick(1);
+        expect(callback).to.have.been.calledOnceWith(1);
+    });
+    it(`passes the last trigger's arguments at maxWaitTime`, () => {
+        const throttle = new Debouncer(callback, 2, 6);
         for (let i = 0; i < 6; i++) {
-            timeoutMock.tick();
-            // eslint-disable-next-line @typescript-eslint/no-floating-promises
-            throttle.trigger();
+            clock.tick(1);
+            promises.push(throttle.trigger(i));
         }
-        expect(spy).to.have.not.been.called;
-        timeoutMock.tick();
-        expect(spy.callCount).to.equal(1);
-        for (let i = 0; i < 3; i++) {
-            timeoutMock.tick();
-            // eslint-disable-next-line @typescript-eslint/no-floating-promises
-            throttle.trigger();
-        }
-        expect(spy.callCount).to.equal(1);
+        expect(callback).to.have.callCount(0);
+        clock.tick(1);
+        expect(callback).to.have.been.calledOnceWith(5);
     });
 });
