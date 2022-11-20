469e181ab02f18978c488a6611df7e22ab85bf96
Omry Nachman <omry@wix.com>
2022-11-20 12:10:01 +0200

    updated prettify step

--- packages/docs/src/build-docs/build-docs.ts
+++ packages/docs/src/build-docs/build-docs.ts
@@ -1,13 +1,14 @@
 /* eslint-disable no-console */
 import type { Config } from '../common';
 import type { Macros } from '../macros.types';
-import { analyze, evaluateMacros, generateMarkdown } from './build-steps';
+import { analyze, evaluateMacros, generateMarkdown, prettify } from './build-steps';
 export type { Macros };
 
 const all = {
     analyze: true,
     evaluateMacros: true,
     generateMarkdown: true,
+    prettify: true
 }
 
 export type BuildSteps = Partial<typeof all>
@@ -17,8 +18,9 @@ export type BuildSteps = Partial<typeof all>
  */
 export function buildDocs(config: Config, steps: BuildSteps = all, macros?: Macros) {
     steps = { ...all, ...steps }
-    if (!steps.analyze) analyze(config);
-    if (!steps.generateMarkdown) generateMarkdown(config);
-    if (!steps.evaluateMacros) evaluateMacros(config, macros);
+    if (steps.analyze) analyze(config);
+    if (steps.generateMarkdown) generateMarkdown(config);
+    if (steps.evaluateMacros) evaluateMacros(config, macros);
+    if (steps.prettify) prettify(config)
 }
 
