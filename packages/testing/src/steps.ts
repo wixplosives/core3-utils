465e62227558daab9ec97ac918b907c9fc4c2b07
Omry Nachman <omry@wix.com>
2022-11-27 19:35:52 +0200

    using useSafeFakeTimers

--- packages/testing/src/steps.ts
+++ packages/testing/src/steps.ts
@@ -1,11 +1,10 @@
 import { timeout } from 'promise-assist';
 
-
 /**
  * A test step
  * @see runSteps
  */
-export type Step<T=unknown> = {
+export type Step<T = unknown> = {
     timeout: number;
     description: string;
     action: Promise<T>;
@@ -19,16 +18,16 @@ export function step<T>(action: Promise<T>, timeout = 1000, description = 'none'
     return {
         action,
         timeout,
-        description
+        description,
     };
 }
 
 /**
- * 
+ *
  * @param steps a steps generator function
  * Creates a test that runs each yielded step with its own timeout and description
- * 
- * @example 
+ *
+ * @example
  * ```ts (runSteps)
  *  it('runs the steps', runSteps(function*(){
  *      expect(yield step(Promise.resolve(1), 10, "prep")).to.equal(1)
@@ -38,19 +37,18 @@ export function step<T>(action: Promise<T>, timeout = 1000, description = 'none'
  */
 export function runSteps(steps: () => Generator<Step, any, any>): (this: Mocha.Context) => Promise<void> {
     return async function () {
-        const gen = steps()
-        let val
-        let next:IteratorResult<Step<unknown>, any>;
+        const gen = steps();
+        let val;
+        let next: IteratorResult<Step<unknown>, any>;
         do {
-            next = gen.next(val)
+            next = gen.next(val);
             if (!next.done) {
-                const s:Step = next.value 
-                val = await timeout(s.action, s.timeout, `Failed in step "${s.description}" after ${s.timeout}ms`)
+                const s: Step = next.value;
+                val = await timeout(s.action, s.timeout, `Failed in step "${s.description}" after ${s.timeout}ms`);
             } else {
                 // eslint-disable-next-line @typescript-eslint/no-unsafe-return
-                return next.value
+                return next.value;
             }
-        } while(!next.done)
+        } while (!next.done);
     };
 }
-
--- packages/testing/src/test/steps.unit.ts
+++ packages/testing/src/test/steps.unit.ts
@@ -1,64 +1,72 @@
-import { sleep } from "promise-assist";
-import { runSteps, step } from "../steps";
-import Sinon from 'sinon'
-import { expect, use } from 'chai'
-import asPromised from 'chai-as-promised'
+import { sleep } from 'promise-assist';
+import { runSteps, step } from '../steps';
+import { expect, use } from 'chai';
+import asPromised from 'chai-as-promised';
+import { useSafeFakeTimers } from '../safe-fake-timer';
 
-use(asPromised)
+use(asPromised);
 
-describe('runSteps', ()=>{
-    let clock:Sinon.SinonFakeTimers;
-    beforeEach(()=>{
-        clock = Sinon.useFakeTimers()
-    })
-    afterEach(()=>{
-        clock.restore()
-    })
-
-    it('waits for the steps to finish', async function() {
-        const result:number[] = [];
-        const test = runSteps(function*(){
-            yield step(sleep(100).then(()=>{result.push(0)}))
-            yield step(sleep(100).then(()=>{result.push(1)}))
-            yield step(sleep(100).then(()=>{result.push(2)}))
-        })
-        const t:Promise<void> = test.bind(this)() 
-        expect(result).to.eql([])
-        await clock.tickAsync(100)
-        expect(result).to.eql([0])
-        await clock.tickAsync(100)
-        expect(result).to.eql([0,1])
-        await clock.tickAsync(100)
-        expect(result).to.eql([0,1,2])
-        return t
-    })
+describe('runSteps', () => {
+    const clock = useSafeFakeTimers();
+    it('waits for the steps to finish', async function () {
+        const result: number[] = [];
+        const test = runSteps(function* () {
+            yield step(
+                sleep(100).then(() => {
+                    result.push(0);
+                })
+            );
+            yield step(
+                sleep(100).then(() => {
+                    result.push(1);
+                })
+            );
+            yield step(
+                sleep(100).then(() => {
+                    result.push(2);
+                })
+            );
+        });
+        const t: Promise<void> = test.bind(this)();
+        expect(result).to.eql([]);
+        await clock.tickAsync(100);
+        expect(result).to.eql([0]);
+        await clock.tickAsync(100);
+        expect(result).to.eql([0, 1]);
+        await clock.tickAsync(100);
+        expect(result).to.eql([0, 1, 2]);
+        return t;
+    });
     it('fails with the step description when a step times out', async function () {
-        const test = runSteps(function*(){
-            yield step(sleep(100), 10, 'wait too long')
-        })
-        const t:Promise<void> = test.bind(this)() 
+        const test = runSteps(function* () {
+            yield step(sleep(100), 10, 'wait too long');
+        });
+        const t: Promise<void> = test.bind(this)();
         // by returning this expectation the promise rejection is not dangling
-        const exp =  expect(t).to.eventually.be.rejectedWith('Failed in step "wait too long" after 10ms')
-        await clock.tickAsync(11)
-        return exp
-    })
+        const exp = expect(t).to.eventually.be.rejectedWith('Failed in step "wait too long" after 10ms');
+        await clock.tickAsync(11);
+        return exp;
+    });
     it('yields the result of a step', async function () {
-        const test = runSteps(function*(){
-            const res = (yield step(Promise.resolve('success'))) as string
-            expect(res).to.equal('success')
-        })
-        const t:Promise<void> = test.bind(this)() 
-        await clock.tickAsync(1)
-        return t
-    })
+        const test = runSteps(function* () {
+            const res = (yield step(Promise.resolve('success'))) as string;
+            expect(res).to.equal('success');
+        });
+        const t: Promise<void> = test.bind(this)();
+        await clock.tickAsync(1);
+        return t;
+    });
 
-    describe('example',()=>{
-        const fetchServerData = ()=>Promise.resolve('server data')
+    describe('example', () => {
+        const fetchServerData = () => Promise.resolve('server data');
         // {@label runSteps
-        it('runs the steps', runSteps(function*(){
-            expect(yield step(Promise.resolve(1), 10, "prep")).to.equal(1)
-            expect(yield step(fetchServerData(), 100, "get data from server")).to.equal("server data")
-        }))
+        it(
+            'runs the steps',
+            runSteps(function* () {
+                expect(yield step(Promise.resolve(1), 10, 'prep')).to.equal(1);
+                expect(yield step(fetchServerData(), 100, 'get data from server')).to.equal('server data');
+            })
+        );
         // @}
-    })
-})
+    });
+});
