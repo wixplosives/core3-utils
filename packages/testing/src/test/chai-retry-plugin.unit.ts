bcdaffa8e571f5148994278ecab13ad5f6709a48
vladyslav-baliuk <vladyslav.b@wix.com>
2023-04-07 19:55:21 +0300

    fix

--- packages/testing/src/test/chai-retry-plugin.unit.ts
+++ packages/testing/src/test/chai-retry-plugin.unit.ts
@@ -6,26 +6,26 @@ Chai.use(chaiRetryPlugin);
 
 describe('chai-retry-plugin', () => {
     it('should retry a function that eventually succeeds', async () => {
-        const { funcToRetry, getCallCount } = withCallCount((callCount: number) => {
+        const { resultFunction, getCallCount } = withCallCount((callCount: number) => {
             if (callCount < 3) {
                 throw new Error('Failed');
             }
             return 'Success';
         });
 
-        await expect(funcToRetry).to.retry().to.equal('Success');
+        await expect(resultFunction).to.retry().to.equal('Success');
         expect(getCallCount()).to.equal(3);
     });
 
     describe('options should work correctly:', () => {
         it('timeout after the specified duration', async () => {
-            const funcToRetry = async () => {
+            const resultFunction = async () => {
                 await sleep(250);
                 return 'Success';
             };
 
             try {
-                await expect(funcToRetry).retry({ timeout: 700 }).to.equal('Failure');
+                await expect(resultFunction).retry({ timeout: 700 }).to.equal('Failure');
                 throw new Error('This should not be called');
             } catch (error: unknown) {
                 expect((error as Error).message).includes('Timed out after 700ms');
@@ -33,10 +33,10 @@ describe('chai-retry-plugin', () => {
         });
 
         it('throw an error when retries limit exceeded', async () => {
-            const { funcToRetry } = withCallCount((callCount) => callCount);
+            const { resultFunction } = withCallCount((callCount) => callCount);
 
             try {
-                await expect(funcToRetry).retry({ retries: 10 }).to.be.above(100);
+                await expect(resultFunction).retry({ retries: 10 }).to.be.above(100);
                 throw new Error('This should not be called');
             } catch (error: unknown) {
                 expect((error as Error).message).to.includes('Limit of 10 retries exceeded!');
@@ -44,12 +44,12 @@ describe('chai-retry-plugin', () => {
         });
 
         it('should apply delay correctly', async () => {
-            const { funcToRetry, getCallCount } = withCallCount(() => {
+            const { resultFunction, getCallCount } = withCallCount(() => {
                 throw new Error('Im throwing');
             });
 
             try {
-                await expect(funcToRetry).retry({ delay: 50, timeout: 500 }).to.equal(5);
+                await expect(resultFunction).retry({ delay: 50, timeout: 500 }).to.equal(5);
             } catch (error: unknown) {
                 expect((error as Error).message).includes('Timed out after 500ms. AssertionError: Error: Im throwing');
                 expect(getCallCount()).to.be.within(9, 10, `Elapsed time should be within 9-10`);
@@ -59,19 +59,19 @@ describe('chai-retry-plugin', () => {
 
     describe('should work with negated assertions:', () => {
         it('assert object with a property `status` that does not match a specific value', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) => ({
+            const { resultFunction, getCallCount } = withCallCount((callCount) => ({
                 status: callCount === 3 ? 'success' : 'pending',
             }));
 
-            await expect(funcToRetry).retry().and.have.property('status').and.not.equal('pending');
+            await expect(resultFunction).retry().and.have.property('status').and.not.equal('pending');
 
             expect(getCallCount()).to.equal(3);
         });
 
         it('assert number with `not` on the beginning of chain', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) => callCount);
+            const { resultFunction, getCallCount } = withCallCount((callCount) => callCount);
 
-            await expect(funcToRetry).retry().to.not.lessThanOrEqual(5).to.equal(6);
+            await expect(resultFunction).retry().to.not.lessThanOrEqual(5).to.equal(6);
 
             expect(getCallCount()).to.equal(7);
         });
@@ -79,12 +79,12 @@ describe('chai-retry-plugin', () => {
         it('assert array that does not include a specific element', async () => {
             const array: number[] = [1, 2, 3, 4, 5];
 
-            const { funcToRetry, getCallCount } = withCallCount(() => {
+            const { resultFunction, getCallCount } = withCallCount(() => {
                 array.shift();
                 return array;
             });
 
-            await expect(funcToRetry).retry({ retries: 4 }).to.not.include(4);
+            await expect(resultFunction).retry({ retries: 4 }).to.not.include(4);
 
             expect(getCallCount()).to.equal(4);
         });
@@ -92,11 +92,11 @@ describe('chai-retry-plugin', () => {
 
     describe('should work with chained properties:', () => {
         it('.deep', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) =>
+            const { resultFunction, getCallCount } = withCallCount((callCount) =>
                 callCount !== 5 ? null : { c: { b: { a: 1 } } }
             );
 
-            await expect(funcToRetry)
+            await expect(resultFunction)
                 .retry()
                 .to.deep.equal({ c: { b: { a: 1 } } });
 
@@ -104,11 +104,11 @@ describe('chai-retry-plugin', () => {
         });
 
         it('.nested', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) =>
+            const { resultFunction, getCallCount } = withCallCount((callCount) =>
                 callCount !== 5 ? null : { a: { b: ['x', 'y'] } }
             );
 
-            await expect(funcToRetry).retry().to.have.nested.property('a.b[1]');
+            await expect(resultFunction).retry().to.have.nested.property('a.b[1]');
 
             expect(getCallCount()).to.equal(5);
         });
@@ -116,40 +116,42 @@ describe('chai-retry-plugin', () => {
 
     describe('should pass the assertions that ends with properties:', () => {
         it('.null', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) => (callCount !== 5 ? null : 'not-null'));
+            const { resultFunction, getCallCount } = withCallCount((callCount) =>
+                callCount !== 5 ? null : 'not-null'
+            );
 
-            await expect(funcToRetry).retry().to.be.not.null;
+            await expect(resultFunction).retry().to.be.not.null;
 
             expect(getCallCount()).to.equal(5);
         });
 
         it('.undefined', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) =>
+            const { resultFunction, getCallCount } = withCallCount((callCount) =>
                 callCount !== 5 ? 'not-undefined' : undefined
             );
 
-            await expect(funcToRetry).retry().to.be.undefined;
+            await expect(resultFunction).retry().to.be.undefined;
 
             expect(getCallCount()).to.equal(5);
         });
 
         it('.empty', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) => (callCount !== 5 ? [1, 2, 3] : []));
+            const { resultFunction, getCallCount } = withCallCount((callCount) => (callCount !== 5 ? [1, 2, 3] : []));
 
-            await expect(funcToRetry).retry().to.be.empty;
+            await expect(resultFunction).retry().to.be.empty;
 
             expect(getCallCount()).to.equal(5);
         });
 
         it('.property(), .true', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) => {
+            const { resultFunction, getCallCount } = withCallCount((callCount) => {
                 if (callCount < 3) {
                     throw new Error('Failed');
                 }
                 return { success: true };
             });
 
-            await expect(funcToRetry).retry().and.have.property('success').and.be.true;
+            await expect(resultFunction).retry().and.have.property('success').and.be.true;
 
             expect(getCallCount()).to.equal(3);
         });
@@ -173,11 +175,11 @@ describe('chai-retry-plugin', () => {
 
     describe('should work with various assertion methods:', () => {
         it('.satisfy()', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) => ({
+            const { resultFunction, getCallCount } = withCallCount((callCount) => ({
                 callCount,
             }));
 
-            await expect(funcToRetry)
+            await expect(resultFunction)
                 .retry()
                 .to.satisfy((obj: { callCount: number }) => obj.callCount > 3);
 
@@ -185,42 +187,42 @@ describe('chai-retry-plugin', () => {
         });
 
         it('.have.property(), and.be.above()', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) =>
+            const { resultFunction, getCallCount } = withCallCount((callCount) =>
                 callCount < 4 ? { notValue: 2 } : { value: callCount }
             );
 
-            await expect(funcToRetry).retry().have.property('value').and.be.above(4);
+            await expect(resultFunction).retry().have.property('value').and.be.above(4);
 
             expect(getCallCount()).to.equal(5);
         });
 
         it('.to.increase(), .by()', async () => {
             const myObj = { val: 1 };
-            const { funcToRetry, getCallCount } = withCallCount(() => {
+            const { resultFunction, getCallCount } = withCallCount(() => {
                 myObj.val += 2;
             });
 
-            await expect(funcToRetry).retry().to.increase(myObj, 'val').by(2);
+            await expect(resultFunction).retry().to.increase(myObj, 'val').by(2);
 
             expect(getCallCount()).to.equal(1);
         });
 
         it('.oneOf()', async () => {
-            const { funcToRetry, getCallCount } = withCallCount((callCount) => callCount);
+            const { resultFunction, getCallCount } = withCallCount((callCount) => callCount);
 
-            await expect(funcToRetry).retry().to.not.be.oneOf([1, 2, 3]);
+            await expect(resultFunction).retry().to.not.be.oneOf([1, 2, 3]);
             expect(getCallCount()).to.equal(4);
         });
 
         it('.change()', async () => {
             const myObj = { dots: '', comas: '' };
 
-            const { funcToRetry, getCallCount } = withCallCount(() => {
+            const { resultFunction, getCallCount } = withCallCount(() => {
                 myObj.dots += '.';
             });
 
-            await expect(funcToRetry).retry().to.change(myObj, 'dots');
-            await expect(funcToRetry).retry().to.not.change(myObj, 'comas');
+            await expect(resultFunction).retry().to.change(myObj, 'dots');
+            await expect(resultFunction).retry().to.not.change(myObj, 'comas');
 
             expect(getCallCount()).to.equal(2);
         });
@@ -236,7 +238,7 @@ const withCallCount = (func: (callCount: number) => unknown) => {
     };
 
     return {
-        funcToRetry: wrapperFunc,
+        resultFunction: wrapperFunc,
         getCallCount: () => callCount,
     };
 };
