6d87a174664ab2d95336f11331b16b8e45676d37
Barak Igal <baraki@wix.com>
2023-05-04 15:21:31 +0300

    * split plugable type * use getThrow * improve set default value perf

--- packages/plugable/src/plugable.ts
+++ packages/plugable/src/plugable.ts
@@ -1,96 +1,98 @@
-import { type Plugable, type Key, internals, PlugableInternals, Val } from './types';
+import { type Plugable, type Key, internals, PlugableInternals, Val, PlugableApi } from './types';
 
-const proto = {
-  get<Value>(this: Plugable, key: Key<Value>): Value | undefined {
-    return get(this, key);
-  },
-  set<Value>(
-    this: Plugable,
-    key: Key<Value>,
-    value: Value,
-    isEqual?: (previous: Value | undefined, value: Value) => boolean
-  ) {
-    set(this, key, value, isEqual);
-  },
-  on<K extends Key>(this: Plugable, key: K, listener: (value: Val<K>) => void) {
-    return on(this, key, listener);
-  },
+const proto: PlugableApi = {
+    getThrow<Value>(this: Plugable, key: Key<Value>): Value {
+        return getThrow(this, key);
+    },
+    get<Value>(this: Plugable, key: Key<Value>): Value | undefined {
+        return get(this, key);
+    },
+    set<Value>(
+        this: Plugable,
+        key: Key<Value>,
+        value: Value,
+        isEqual?: (previous: Value | undefined, value: Value) => boolean
+    ) {
+        set(this, key, value, isEqual);
+    },
+    on<K extends Key>(this: Plugable, key: K, listener: (value: Val<K>) => void) {
+        return on(this, key, listener);
+    },
 };
 
 export function createPlugable(): Plugable {
-  const rec = Object.create(proto) as Plugable;
-  rec[internals] = {
-    parent: undefined,
-    listeners: new Map(),
-    handlerToRec: new WeakMap(),
-  };
-  return rec;
+    const rec = Object.create(proto) as Plugable;
+    rec[internals] = {
+        parent: undefined,
+        listeners: new Map(),
+        handlerToRec: new WeakMap(),
+    };
+    return rec;
 }
 
-export function inheritPlugable(rec: Plugable) {
-  const inherited = Object.create(rec) as Plugable;
-  inherited[internals] = Object.create(rec[internals]) as PlugableInternals;
-  inherited[internals].parent = rec;
-  return inherited;
+export function inheritPlugable(rec: Plugable): Plugable {
+    const inherited = Object.create(rec) as Plugable;
+    inherited[internals] = Object.create(rec[internals]) as PlugableInternals;
+    inherited[internals].parent = rec;
+    return inherited;
 }
 
 export function createKey<V = never>(debugName?: string): Key<V> {
-  return Symbol(debugName) as Key<V>;
+    return Symbol(debugName) as Key<V>;
 }
 
 export function getThrow<Value>(rec: Plugable, key: Key<Value>): Value {
-  const value = get(rec, key);
-  if (value === undefined || value === null) {
-    throw new Error(`missing value for key ${String(key)}`);
-  }
-  return value;
+    const value = get(rec, key);
+    if (value === undefined || value === null) {
+        throw new Error(`missing value for key ${String(key)}`);
+    }
+    return value;
 }
 
 export function get<Value>(rec: Plugable, key: Key<Value>): Value | undefined {
-  return (rec as Record<Key<Value>, Value>)[key];
+    return (rec as Record<Key<Value>, Value>)[key];
 }
 
-export function set<Value>(
-  rec: Plugable,
-  key: Key<Value>,
-  value: Value,
-  isEqual = (previous: Value | undefined, value: Value) => previous === value
-) {
-  if (!isEqual(get(rec, key), value)) {
-    (rec as Record<Key<Value>, Value>)[key] = value;
-    dispatch(rec, key, value);
-  }
+export function set<Value>(rec: Plugable, key: Key<Value>, value: Value, isEqual = tripleEqual<Value>): void {
+    if (!isEqual(get(rec, key), value)) {
+        (rec as Record<Key<Value>, Value>)[key] = value;
+        dispatch(rec, key, value);
+    }
 }
 
-export function on<K extends Key>(rec: Plugable, key: K, listener: (value: Val<K>) => void) {
-  const { listeners, handlerToRec } = rec[internals];
-  let handlers = listeners.get(key);
-  if (!handlers) {
-    handlers = new Set();
-    listeners.set(key, handlers);
-  }
-  handlers.add(listener);
-  handlerToRec.set(listener, rec);
-  return () => {
-    handlers?.delete(listener);
-  };
+export function on<K extends Key>(rec: Plugable, key: K, listener: (value: Val<K>) => void): () => void {
+    const { listeners, handlerToRec } = rec[internals];
+    let handlers = listeners.get(key);
+    if (!handlers) {
+        handlers = new Set();
+        listeners.set(key, handlers);
+    }
+    handlers.add(listener);
+    handlerToRec.set(listener, rec);
+    return () => {
+        handlers?.delete(listener);
+    };
 }
 
-function dispatch<T>(rec: Plugable, key: Key<T>, value: T) {
-  const { listeners, handlerToRec } = rec[internals];
-  listeners.get(key)?.forEach((listener) => shouldDispatch(rec, handlerToRec.get(listener), key) && listener(value));
+function tripleEqual<T>(previous: T | undefined, value: T): boolean {
+    return previous === value;
 }
 
-function hasOwn(obj: unknown, key: string | symbol) {
-  return Object.prototype.hasOwnProperty.call(obj, key);
+function dispatch<T>(rec: Plugable, key: Key<T>, value: T): void {
+    const { listeners, handlerToRec } = rec[internals];
+    listeners.get(key)?.forEach((listener) => shouldDispatch(rec, handlerToRec.get(listener), key) && listener(value));
+}
+
+function hasOwn(obj: unknown, key: string | symbol): boolean {
+    return Object.prototype.hasOwnProperty.call(obj, key);
 }
 
 function shouldDispatch(dispatcherRec: Plugable, handlerRec: Plugable | undefined, key: string | symbol): boolean {
-  if (dispatcherRec === handlerRec) {
-    return true;
-  } else if (handlerRec && !hasOwn(handlerRec, key)) {
-    return shouldDispatch(dispatcherRec, handlerRec[internals].parent, key);
-  } else {
-    return false;
-  }
+    if (dispatcherRec === handlerRec) {
+        return true;
+    } else if (handlerRec && !hasOwn(handlerRec, key)) {
+        return shouldDispatch(dispatcherRec, handlerRec[internals].parent, key);
+    } else {
+        return false;
+    }
 }
--- packages/plugable/src/types.ts
+++ packages/plugable/src/types.ts
@@ -5,17 +5,20 @@ export type Key<V = unknown> = symbol & { [secret]: V | undefined };
 export type Val<T> = T extends Key<infer V> ? V : never;
 
 export type PlugableInternals = {
-  parent: Plugable | undefined;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly listeners: Map<Key, Set<(value: any) => void>>;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  readonly handlerToRec: WeakMap<(value: any) => void, Plugable>;
+    parent: Plugable | undefined;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    readonly listeners: Map<Key, Set<(value: any) => void>>;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    readonly handlerToRec: WeakMap<(value: any) => void, Plugable>;
 };
 
-export type Plugable = {
-  get<Value>(key: Key<Value>): Value | undefined;
-  set<Value>(key: Key<Value>, value: Value, isEqual?: (previous: Value | undefined, value: Value) => boolean): void;
-  on<K extends Key>(key: K, listener: (value: Val<K>) => void): () => void;
-  [internals]: PlugableInternals;
+export type PlugableApi = {
+    getThrow<Value>(key: Key<Value>): Value;
+    get<Value>(key: Key<Value>): Value | undefined;
+    set<Value>(key: Key<Value>, value: Value, isEqual?: (previous: Value | undefined, value: Value) => boolean): void;
+    on<K extends Key>(key: K, listener: (value: Val<K>) => void): () => void;
 };
 
+export type Plugable = PlugableApi & {
+    [internals]: PlugableInternals;
+};
