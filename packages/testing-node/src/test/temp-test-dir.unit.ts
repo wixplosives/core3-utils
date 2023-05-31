b22d502748c0aee64d18a847f7cee52c4fc01d8d
vladyslav-baliuk <vladyslav.b@wix.com>
2023-05-31 16:15:22 +0300

    fix

--- packages/testing-node/src/test/temp-test-dir.unit.ts
+++ packages/testing-node/src/test/temp-test-dir.unit.ts
@@ -1,4 +1,4 @@
-import _fs from '@file-services/node';
+import fs from '@file-services/node';
 import { expect } from 'chai';
 import { createTestDir } from '../temp-test-dir';
 
@@ -6,9 +6,9 @@ describe('createTestDir', () => {
     let dirPath: string;
     it('creates a temporary directory', () => {
         dirPath = createTestDir('test');
-        expect(_fs.statSync(dirPath, { throwIfNoEntry: true }).isDirectory()).to.equal(true);
+        expect(fs.statSync(dirPath, { throwIfNoEntry: true }).isDirectory()).to.equal(true);
     });
     it('deletes the temporary directory', () => {
-        expect(_fs.existsSync(dirPath)).to.equal(false);
+        expect(fs.existsSync(dirPath)).to.equal(false);
     });
 });
