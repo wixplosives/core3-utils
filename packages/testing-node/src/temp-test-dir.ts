d772e3c0876b771e5249da739a8771704cd068e4
Omry Nachman <omry@wix.com>
2023-06-24 15:10:31 +0300

    - chore!: added disposable options to createTestDir - chore: added timeout and name to removal

--- packages/testing-node/src/temp-test-dir.ts
+++ packages/testing-node/src/temp-test-dir.ts
@@ -1,6 +1,8 @@
 import { createTempDirectorySync } from 'create-temp-directory';
 import { createDisposalGroup, disposeAfter, DEFAULT_DISPOSAL_GROUP } from '@wixc3/testing';
 import fs from '@file-services/node';
+import { DisposableOptions } from '@wixc3/patterns';
+import { defaults } from '@wixc3/common';
 export const DISPOSE_OF_TEMP_DIRS = 'DISPOSE_OF_TEMP_DIRS';
 try {
     createDisposalGroup(DISPOSE_OF_TEMP_DIRS, { after: DEFAULT_DISPOSAL_GROUP });
@@ -14,8 +16,21 @@ try {
  * The directory will be deleted after the test, thus not suitable for suites ("describe")
  * @returns temp directory path
  */
-export function createTestDir(prefix?: string | undefined, disposalGroup = DISPOSE_OF_TEMP_DIRS) {
+export function createTestDir(
+    prefix?: string | undefined,
+    disposalOptions: DisposableOptions | string = DISPOSE_OF_TEMP_DIRS
+) {
     const dir = createTempDirectorySync(prefix);
-    disposeAfter(() => dir.remove(), { group: disposalGroup });
+    const options = defaults<DisposableOptions, DisposableOptions>(
+        typeof disposalOptions === 'string'
+            ? {
+                  group: disposalOptions,
+                  name: `removing test dir: ${dir.path}`,
+                  timeout: 2_000,
+              }
+            : disposalOptions,
+        { group: DISPOSE_OF_TEMP_DIRS }
+    );
+    disposeAfter(() => dir.remove(), options);
     return fs.realpathSync.native(dir.path);
 }
