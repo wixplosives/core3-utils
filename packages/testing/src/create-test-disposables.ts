a444264f68567ee8b115be81c7fecbe2a370d039
Barak <baraki@wix.com>
2023-12-06 16:42:42 +0200

    docs: added readme entry

--- packages/testing/src/create-test-disposables.ts
+++ packages/testing/src/create-test-disposables.ts
@@ -3,14 +3,14 @@ import { mochaCtx } from './mocha-ctx';
 
 /**
  * Creates a disposable group that will be disposed after the test is done
- * @param disposeHook mocha hook to dispose the group, defaults to afterEach
+ * @param disposeHook mocha hook to dispose the group, defaults to "afterEach" passing "after" will dispose after all tests
  */
 
 export function createTestDisposables(disposeHook = afterEach) {
-  if (mochaCtx()?.currentTest) {
-    throw new Error('createMochaDisposables must not be called inside a test only at describe or module');
-  }
-  const disposables = createDisposables();
-  disposeHook('createTestDisposables', () => disposables.dispose());
-  return disposables;
+    if (mochaCtx()?.currentTest) {
+        throw new Error('createMochaDisposables must not be called inside a test only at describe or module');
+    }
+    const disposables = createDisposables();
+    disposeHook('createTestDisposables', () => disposables.dispose());
+    return disposables;
 }
