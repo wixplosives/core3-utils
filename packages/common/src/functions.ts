18be722b6a269e46506af7ba7b214e160cb3ff7b
Omry Nachman <omry@wix.com>
2024-01-23 11:46:34 +0100

    chore: added memoize

--- packages/common/src/functions.ts
+++ packages/common/src/functions.ts
@@ -90,3 +90,24 @@ export function enforceSequentialExecution<P, T extends (...args: any[]) => Prom
         return queue;
     }) as T;
 }
+
+/**
+ * 
+ * @param fn a function to memoize
+ * @param argsHash a function that returns a string hash for the arguments, defaults to JSON.stringify
+ * @returns a memoized version of `fn`
+ */
+export function memoize<T extends (...args: any[]) => any>(
+    fn: T,
+    argsHash: (args: Parameters<T>) => string = JSON.stringify
+): T {
+    const cache = new Map<string, ReturnType<T>>();
+    return ((...args: Parameters<T>) => {
+        const key = argsHash(args);
+        if (!cache.has(key)) {
+            cache.set(key, fn(...args) as ReturnType<T>);
+        }
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+        return cache.get(key);
+    }) as T;
+}
--- packages/common/src/test/functions.unit.ts
+++ packages/common/src/test/functions.unit.ts
@@ -1,5 +1,5 @@
 import chai, { expect } from 'chai';
-import { delayed, enforceSequentialExecution, once } from '..';
+import { delayed, enforceSequentialExecution, memoize, once } from '..';
 import Sinon, { stub } from 'sinon';
 import sinonChai from 'sinon-chai';
 import { sleep } from 'promise-assist';
@@ -82,5 +82,33 @@ describe('delayed', () => {
         await clock.tickAsync(100);
         expect(await r1).to.equal(1);
         expect(await r2).to.equal(2);
+    });
+});
+
+describe('memoize', () => {
+    it('runs fn only once per given args', () => {
+        let callCount = 0;
+        const fn = (num: number) => {
+            callCount++;
+            return `${num}-${callCount}`;
+        };
+        const memoized = memoize(fn);
+        expect(memoized(1)).to.equal('1-1');
+        expect(memoized(1)).to.equal('1-1');
+        expect(memoized(2)).to.equal('2-2');
+        expect(memoized(2)).to.equal('2-2');
+    });
+    describe('with a custom cache', () => {
+        let callCount = 0;
+        const fn = (num: number, ..._args: any[]) => {
+            callCount++;
+            return `${num}-${callCount}`;
+        };
+        const hashOnlyFirstArg = (args: any[]) => `${args[0]}`;
+        const memoized = memoize(fn, hashOnlyFirstArg);
+        expect(memoized(1, 1)).to.equal('1-1');
+        expect(memoized(1, 2)).to.equal('1-1');
+        expect(memoized(2, 1)).to.equal('2-2');
+        expect(memoized(2, 2)).to.equal('2-2');
     });
 });
