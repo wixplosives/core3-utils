92d0d34eb31092d2cb6d17b5a62c03162109773d
Omry Nachman <omry@wix.com>
2024-01-31 14:11:23 +0100

    removed code duplication

--- packages/testing/src/chai-retry-plugin/types.ts
+++ packages/testing/src/chai-retry-plugin/types.ts
@@ -1,3 +1,5 @@
+import { PromiseLikeAssertion } from '../types';
+
 declare global {
     // eslint-disable-next-line @typescript-eslint/no-namespace
     namespace Chai {
@@ -46,14 +48,3 @@ export type RetryOptions = {
     delay?: number;
     timeout?: number;
 };
-
-// Helper type to convert a type T into a Promise-like version of itself
-type Promisify<T> = {
-    [Key in keyof T]: T[Key] extends T
-        ? Promisify<T[Key]> & PromiseLike<any>
-        : T[Key] extends (...args: any) => any
-          ? (...args: Parameters<T[Key]>) => Promisify<ReturnType<T[Key]>> & PromiseLike<any>
-          : Promisify<T[Key]> & PromiseLike<any>;
-};
-
-export type PromiseLikeAssertion = Promisify<Chai.Assertion> & PromiseLike<void>;
