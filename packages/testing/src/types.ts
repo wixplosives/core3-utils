ec9754bb2b937eee5aced161a0ec82504e819852
Reza Banazadeh <rezab@wix.com>
2024-02-07 15:42:43 +0100

    small type refactor

--- packages/testing/src/types.ts
+++ packages/testing/src/types.ts
@@ -1,11 +1,12 @@
+type PromisifiedType<T> = Promisify<T> & PromiseLike<any>;
+
 // Helper type to convert a type T into a Promise-like version of itself
 type Promisify<T> = {
     [Key in keyof T]: T[Key] extends (...args: any) => any
         ? keyof T[Key] extends never
-            ? (...args: Parameters<T[Key]>) => Promisify<ReturnType<T[Key]>> & PromiseLike<any>
-            : Promisify<T[Key]> &
-                  PromiseLike<any> & { (...args: Parameters<T[Key]>): Promisify<ReturnType<T[Key]>> & PromiseLike<any> }
-        : Promisify<T[Key]> & PromiseLike<any>;
+            ? (...args: Parameters<T[Key]>) => PromisifiedType<ReturnType<T[Key]>>
+            : PromisifiedType<T[Key]> & { (...args: Parameters<T[Key]>): PromisifiedType<ReturnType<T[Key]>> }
+        : PromisifiedType<T[Key]>;
 };
 
 export type PromiseLikeAssertion<T extends Chai.Assertion = Chai.Assertion> = Promisify<T> & PromiseLike<void>;
