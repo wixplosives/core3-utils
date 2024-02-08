cece71cbfeb2dcb0717d9e06981e6ddd162cfab4
Omry Nachman <omry@wix.com>
2024-02-08 12:20:46 +0100

    chore: sorting result by weights

--- packages/patterns/src/partition.ts
+++ packages/patterns/src/partition.ts
@@ -8,24 +8,21 @@
  * @param data
  * @param bucketsCount
  * @param predicate maps a data items to weight
- * @returns
+ * @returns buckets of similar total weight (sorted by weights, descending)
  */
 export function partition<T>(data: T[], bucketsCount: number, predicate: (item: T) => number): T[][] {
     const items = data.toSorted((a, b) => predicate(a) - predicate(b));
-    const buckets = Array.from({ length: bucketsCount }, () => [] as T[]);
-    const totals = Array.from({ length: bucketsCount }, () => 0);
+    const buckets = Array.from({ length: bucketsCount }, () => ({ total: 0, content: [] as T[] }));
 
-    const findSmallest = () => buckets.reduce((acc, _, idx) => (totals[idx]! < totals[acc]! ? idx : acc), 0);
+    const findSmallest = () =>
+        buckets.reduce((smallest, current) => (current.total < smallest.total ? current : smallest), buckets[0]!);
 
     while (items.length > 0) {
         const nextItem = items.pop() as T;
         const smallestBucket = findSmallest();
-        buckets[smallestBucket]!.push(nextItem);
-
-        totals[smallestBucket] += predicate(nextItem);
-
-        buckets[findSmallest()]!.push();
+        smallestBucket.content.push(nextItem);
+        smallestBucket.total += predicate(nextItem);
     }
 
-    return buckets;
+    return buckets.toSorted((a, b) => b.total - a.total).map((bucket) => bucket.content);
 }
