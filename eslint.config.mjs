4030f62975318fd77667586c9d04c9c48612dfd9
Avi Vahl <avi@vahl.co.il>
2024-11-23 00:27:05 +0200

    chore(deps-dev): typescript@5.7.2, latest others and regenerate lock file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,7 +9,7 @@ for (const config of pluginTypescript.configs.recommendedTypeChecked) {
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['**/dist/'] },
+  { ignores: ['**/dist/', 'packages/mostly-equal/mostly-equal-chai.d.ts'] },
   pluginJs.configs.recommended,
   { plugins: { 'no-only-tests': pluginNoOnlyTests } },
   {
