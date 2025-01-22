391ca9e281298d7746a2af33e57c7508b39029e0
Vladyslav Babak <vladyslavba@wix.com>
2025-01-22 11:54:33 +0100

    fix formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,32 +4,32 @@ import pluginNoOnlyTests from 'eslint-plugin-no-only-tests';
 import pluginTypescript from 'typescript-eslint';
 
 for (const config of pluginTypescript.configs.recommendedTypeChecked) {
-  config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
+    config.files = ['**/*.{ts,tsx,mts,cts}']; // ensure config only targets TypeScript files
 }
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { ignores: ['**/dist/', 'packages/mostly-equal/mostly-equal-chai.d.ts'] },
-  pluginJs.configs.recommended,
-  { plugins: { 'no-only-tests': pluginNoOnlyTests } },
-  {
-    rules: {
-      'no-console': 'error',
-      'no-only-tests/no-only-tests': 'error',
-      'no-undef': 'off',
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    { ignores: ['**/dist/', 'packages/mostly-equal/mostly-equal-chai.d.ts'] },
+    pluginJs.configs.recommended,
+    { plugins: { 'no-only-tests': pluginNoOnlyTests } },
+    {
+        rules: {
+            'no-console': 'error',
+            'no-only-tests/no-only-tests': 'error',
+            'no-undef': 'off',
+            'no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+        },
     },
-  },
-  ...pluginTypescript.configs.recommendedTypeChecked,
-  { languageOptions: { parserOptions: { projectService: true } } },
-  {
-    files: ['**/*.{ts,tsx,mts,cts}'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-expressions': 'off',
-      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
-      '@typescript-eslint/restrict-template-expressions': 'off',
+    ...pluginTypescript.configs.recommendedTypeChecked,
+    { languageOptions: { parserOptions: { projectService: true } } },
+    {
+        files: ['**/*.{ts,tsx,mts,cts}'],
+        rules: {
+            '@typescript-eslint/no-explicit-any': 'off',
+            '@typescript-eslint/no-unused-expressions': 'off',
+            '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+            '@typescript-eslint/restrict-template-expressions': 'off',
+        },
     },
-  },
-  configPrettier,
+    configPrettier,
 ];
